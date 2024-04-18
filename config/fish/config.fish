5d605ec1b6dc5c6dde2dc83d65c28938ff7102fb
carakesa <carakesa@gmail.com>
2024-04-17 22:57:40 -0400

    fix manpager for bat

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -66,8 +66,8 @@ set PYTHONDONTWRITEBYTECODE yes # Prevent creation of __pycache__ folders
 ### Uncomment only one of these!
 
 ### "bat" as manpager
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-set MANROFFOPT -c
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+export MANROFFOPT="-c"
 
 ### "vim" as manpager
 # set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
