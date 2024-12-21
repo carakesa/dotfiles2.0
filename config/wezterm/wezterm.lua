03be89efad2cae5109969d9d8d4c101e65d80b73
carakesa <carakesa@gmail.com>
2024-12-21 09:26:45 -0500

    wezterm fix

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -21,14 +21,15 @@ end
 
 -- Settings
 config.default_prog = { fish_path, "-l" }
+config.enable_wayland = false
 
 -- config.color_scheme = "Tokyo Night"
 config.color_scheme = "Hivacruz"
 config.font = wezterm.font_with_fallback({
-	{ family = "IosevkaTermSlab Nerd Font Propo", scale = 1.0, weight = "Light" },
+	{ family = "IosevkaTerm Nerd Font", scale = 1.0, weight = "Light" },
 	{ family = "JetBrains Mono Nerd Font", scale = 1.0 },
 })
-config.window_background_opacity = 0.90
+config.window_background_opacity = 0.60
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
@@ -128,62 +129,22 @@ config.key_tables = {
 
 -- Tab bar
 -- I don't like the look of "fancy" tab bar
-config.use_fancy_tab_bar = false
+config.use_fancy_tab_bar = true
 config.status_update_interval = 1000
 config.tab_bar_at_bottom = false
-wezterm.on("update-status", function(window, pane)
-	-- Workspace name
-	local stat = window:active_workspace()
-	local stat_color = "#f7768e"
-	-- It's a little silly to have workspace name all the time
-	-- Utilize this to display LDR or current key table name
-	if window:active_key_table() then
-		stat = window:active_key_table()
-		stat_color = "#7dcfff"
-	end
-	if window:leader_is_active() then
-		stat = "LDR"
-		stat_color = "#bb9af7"
-	end
-
 	-- Current working directory
-	local basename = function(s)
-		-- Nothing a little regex can't fix
-		return string.gsub(s, "(.*[/\\])(.*)", "%2")
-	end
+	--	local basename = function(s)
+	-- Nothing a little regex can't fix
+	--		return string.gsub(s, "(.*[/\\])(.*)", "%2")
+	--	end
 	-- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l). Not a big deal, but check in case
-	local cwd = pane:get_current_working_dir()
-	cwd = cwd and basename(cwd) or ""
+	--	local cwd = pane:get_current_working_dir()
+	--cwd = cwd and basename(cwd) or ""
 	-- Current command
-	local cmd = pane:get_foreground_process_name()
-	cmd = cmd and basename(cmd) or ""
+	--	local cmd = pane:get_foreground_process_name()
+	--	cmd = cmd and basename(cmd) or ""
 
 	-- Time
-	local time = wezterm.strftime("%H:%M")
-
-	-- Left status (left of the tab line)
-	window:set_left_status(wezterm.format({
-		{ Foreground = { Color = stat_color } },
-		{ Text = "  " },
-		{ Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
-		{ Text = " |" },
-	}))
-
-	-- Right status
-	window:set_right_status(wezterm.format({
-		-- Wezterm has a built-in nerd fonts
-		-- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
-		{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
-		{ Text = " | " },
-		{ Foreground = { Color = "#e0af68" } },
-		{ Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
-		"ResetAttributes",
-		{ Text = " | " },
-		{ Text = wezterm.nerdfonts.md_clock .. "  " .. time },
-		{ Text = "  " },
-	}))
-end)
-
 --[[ Appearance setting for when I need to take pretty screenshots
 config.enable_tab_bar = false
 config.window_padding = {
