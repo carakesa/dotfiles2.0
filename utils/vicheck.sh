b80c7e3ec99cdb487c769914172507f94ccd45ff
ShaneC80 <53915083+carakesa@users.noreply.github.com>
2025-01-12 07:02:36 -0500

    default to neovim (now w/ LazyVIM)

--- utils/vicheck.sh
+++ utils/vicheck.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #Check for lvim, nvim, then vim, then vi, and run;
 
-if [ -f ~/.local/bin/lvim ]; then
-  ~/.local/bin/lvim $1
+if [ -f ~/.local/bin/nvim ]; then
+  ## if [ -f ~/.local/bin/lvim ]; then  **This was the first if for Lunarvim, not currently maintained**
+  "$HOME/.local/bin/lvim $1"
 elif [ -f /sbin/nvim ]; then
   nvim $1
 elif [ -f /sbin/vim ]; then
   vim $1
-elif [-f /sbin/vi ]; then
+elif [ -f /sbin/vi ]; then
   vi $1
 fi
-
